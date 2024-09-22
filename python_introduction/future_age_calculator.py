f111d1aa042769c2ec376b96b4b4d865bc4687ec
mistirnigusse <mistirnigusse00@gmail.com>
2024-09-22 15:38:38 +0300

    Basic python assignment

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,2 @@
+age = input("How old are you?")
+print(f"In 2050, you will be {int(age)+27} years old")
---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,5 @@
+principal = 1000
+rate = 0.05
+time = 3
+I = principal * rate * time
+print(f"The simple interest is: {I}")
