474f0e0de454bb8d558873c982e6c9e9d0eeff6e
mistirnigusse <mistirnigusse00@gmail.com>
2024-09-24 20:45:36 +0300

    solved errors shown by the auto corrector

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,6 +1,6 @@
 X = input("Enter a number to see its multiplication table: ")
 list = [1,2,3,4,5,6,7,8,9,10]
 
-for Y in list:
+for Y in range(10):
     Z = X * Y
     print(f"{X} * {Y} = {Z}")
--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,6 +1,17 @@
+# size = int(input("Enter the size of the pattern: "))
+
+# for i in range(size):
+#     for j in range(size):
+#       print("*", end="")
+#     print()
+
 size = int(input("Enter the size of the pattern: "))
 
-for i in range(size):
-    for j in range(size):
-      print("*", end="")
+i = 0
+while i < size:
+    j = 0
+    while j < size:
+        print("*", end="")
+        j += 1
     print()
+    i += 1
